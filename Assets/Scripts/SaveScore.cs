3c678049124f47c019d59bf726ee74f1a44d76d1
Andres <andres.bd@outlook.com>
2019-09-23 16:03:12 -0500

    initial commit

---
+++ Assets/Scripts/SaveScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase.Sample.Database;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveScore : MonoBehaviour
+{
+    private DBHandler dbManager;
+    private InputField Email, Score;
+
+    public void RegisterScore() {
+
+        dbManager = GameObject.Find("ScoreManager").GetComponent<DBHandler>();
+        Email = GameObject.Find("EmailInput").GetComponent<InputField>();
+        Score = GameObject.Find("ScoreInput").GetComponent<InputField>();
+        dbManager.email = Email.text;
+        dbManager.score = System.Convert.ToInt32(Score.text);
+
+        dbManager.AddScore();
+    }
+}
